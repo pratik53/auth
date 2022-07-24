2a87c33853ef52a5d733b0a78bec9051ef78a5b0
Patya <pratiksheaonkar@gmail.com>
2022-07-24 15:13:05 +0530

    git ignore added

--- User/views.py
+++ User/views.py
@@ -57,6 +57,7 @@ class UserPasswordChangeView(APIView):
         sdata = UserChangePasswordSerializer(data=request.data,context = {'user':data})
         sdata.is_valid(raise_exception=True)
         sdata.save()
+        print('hello world')
         return Response(status = status.HTTP_200_OK)
 
         
